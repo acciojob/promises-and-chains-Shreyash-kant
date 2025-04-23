7bae005985b9db1a5f0257f54514fd8579c73adf
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2025-04-23 12:52:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //your JS code here. If required.
 const form = document.getElementById("userform");
-function checkElegibility(age){
+function checkElegibility(age, name){
 	return new Promise((resolve,reject)=>{
 		if(age>18){
-			setTimeout(()=>resolve("Welcome, . You can vote."),4000);
+			setTimeout(()=>resolve(`Welcome, ${name}. You can vote.`),4000);
 		}
-		else if(age<18) setTimeout(()=>reject("Oh sorry . You aren't old enough."),4000);
+		else if(age<18) setTimeout(()=>reject(`Oh sorry ${name}. You aren't old enough.`),4000);
 	})
 }
 function validate(e){
@@ -14,7 +14,7 @@ function validate(e){
 	let age = parseInt(document.getElementById("age").value.trim());
 	if(name==="" || isNaN(age))
 		alert("Please enter valid details");
-	else checkElegibility(age).then((message)=>{
+	else checkElegibility(age,name).then((message)=>{
 		alert(message);
 	}).catch((message)=>{
 		alert(message);
