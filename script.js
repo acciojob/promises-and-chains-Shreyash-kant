8da7993285a07a0150793c3a9ee9406cd8a20967
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2025-04-23 12:47:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const form = document.getElementById("userform");
+function checkElegibility(age){
+	return new Promise((resolve,reject)=>{
+		if(age>18){
+			setTimeout(()=>resolve("Welcome, . You can vote."),4000);
+		}
+		else if(age<18) setTimeout(()=>reject("Oh sorry . You aren't old enough."),4000);
+	})
+}
+function validate(e){
+	e.preventDefault();
+	let name = document.getElementById("name").value.trim();
+	let age = parseInt(document.getElementById("age").value.trim());
+	if(name==="" || isNaN(age))
+		alert("Please enter valid details");
+	else checkElegibility(age).then((message)=>{
+		alert(message);
+	}).catch((message)=>{
+		alert(message);
+	});
+}
+form.addEventListener("submit",validate)
